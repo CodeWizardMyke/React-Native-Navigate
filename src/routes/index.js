2d277d29b9fdec8ec524fbb69259814a7af5281b
Myke William Oliveira Feitosa <maikewilliam.dev@gmail.com>
2025-02-28 11:56:33 -0300

    FEAT testing and using 2 types navigate stack and tab

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
-
-import { SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, Text, View, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
 import Stack from './Stack'
-//import Tab from './Tab'
+import Tab from './Tab'
 
-export default props => {
+export default function App() {
     const [toggleNavigate, setToggleNavigate] = useState('Stack');
+    const [navType, setNavType] = useState(<Stack />);
+
+    useEffect(() => {
+        setNavType(toggleNavigate === "Stack" ? <Stack /> : <Tab />);
+    }, [toggleNavigate]);
 
     return (
-        <SafeAreaView ew style={{flex:1}} >
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ backgroundColor: "#DDD", padding: 10, alignItems: "center" }}>
+                <Text>Seleção da forma de navegação</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', width: '100%', paddingTop: 15 }}>
+                    <Button title='Stack' onPress={() => setToggleNavigate("Stack")} />
+                    <Button title='Tab' onPress={() => setToggleNavigate("Tab")} />
+                </View>
+            </View>
+
             <NavigationContainer>
-                <Stack/>
+                {navType}
             </NavigationContainer>
         </SafeAreaView>
     )
